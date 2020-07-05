506ac5a826245d5042dffee4569214cc4622dcc8
hiba daoudi <hiiba.daoudi@gmail.com>
2020-07-05 18:01:17 +0100

    V final -1

---
+++ test/crop.php
@@ -0,0 +1,18 @@
+<?php
+$im = imagecreatefrompng('../images/image.png');
+
+
+if(isset($_POST['submit'])) {
+ 
+        $im2=imagecrop($im, ['x' =>$_POST['x1'], 'y' =>  $_POST['y1'], 'width' => $_POST['w'], 'height' => $_POST['h']]);
+        if ($im2 !== FALSE) {
+            imagepng($im2, '../images/image.png');
+            imagedestroy($im2);
+        }
+        imagedestroy($im);
+        
+        
+    }
+    header('location:../acceuil/calc.html');
+
+?>
